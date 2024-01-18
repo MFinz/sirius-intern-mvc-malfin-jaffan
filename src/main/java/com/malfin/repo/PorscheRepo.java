f2a7ce3f7162f273283c84aeb7fee2f8742149ab
MFinz <malfinjaffanwaskito@gmail.com>
2024-01-18 14:22:48 +0700

    feat: Add Porsche CRUD and fix UI Layout

---
+++ src/main/java/com/malfin/repo/PorscheRepo.java
@@ -0,0 +1,10 @@
+package com.malfin.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.malfin.entity.Porsche;
+
+public interface PorscheRepo extends CrudRepository<Porsche, Integer>{
+
+}
+
